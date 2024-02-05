f44ed9ae1a14c2ccbcf1e86f7ec88f4452b2371a
kei ito <ito.kei@jvckenwood.com>
2024-02-05 09:53:14 +0900

    キャプションボタン機能追加

--- WpfApp1/WpfApp1/Window2.xaml.cs
+++ WpfApp1/WpfApp1/Window2.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Window2.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : WindowBase
+    public partial class Window2 : WindowBase
     {
-        public MainWindow()
+        public Window2()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var window2 = new Window2();
-            window2.Show();
         }
     }
 }
---
+++ WpfApp1/WpfApp1/WindowBase.cs
@@ -0,0 +1,61 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public abstract class WindowBase : Window
+    {
+        // Disposeが必要な処理をまとめてやる
+        private CompositeDisposable Disposable { get; } = new();
+
+        // 最小化ボタンが押された時
+        public ReactiveCommand WindowMinimum { get; } = new();
+        // 最大化、通常サイズのボタンが押された時
+        public ReactiveCommand WindowSize { get; } = new();
+        // ウインドウを閉じるボタンが押された時
+        public ReactiveCommand WindowClose { get; } = new();
+        // ウインドウのサイズが変更されるイベントが発生した時(最大化と通常の変化)
+        public ReactiveCommand SizeChangedCommand { get; } = new();
+
+        static WindowBase()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WindowBase), new FrameworkPropertyMetadata(typeof(WindowBase)));
+        }
+
+        public WindowBase()
+        {
+            this.Closing += WindowBase_Closing;
+
+            // 最小化ボタンが押された
+            _ = WindowMinimum.Subscribe(_ => this.WindowState = WindowState.Minimized).AddTo(Disposable);
+            // 最大化、通常サイズボタンが押された
+            _ = WindowSize.Subscribe(_ => this.WindowState = this.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal).AddTo(Disposable);
+            // 閉じるボタンが押された
+            _ = WindowClose.Subscribe(_ => Window.GetWindow(this).Close()).AddTo(Disposable);
+            // ウィンドウサイズが変わった
+            _ = SizeChangedCommand.Subscribe(_ =>
+            {
+            }).AddTo(Disposable);
+        }
+
+        private void WindowBase_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Disposable.Dispose();
+        }
+    }
+}
